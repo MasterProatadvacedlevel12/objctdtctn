84adfb6386c17162f95b41650d6875e491e5ca61
Ronav Sharma <74993217+MasterProatadvacedlevel12@users.noreply.github.com>
2021-09-07 17:58:29 -0700

    Add files via upload

---
+++ bedrm.js
@@ -0,0 +1,15 @@
+img = "";
+
+function preload(){
+    img = loadImage("bedrm.jpg");
+}
+
+
+function setup(){
+    canvas = createCanvas(640,420);
+    canvas.center();
+}
+
+function draw(){
+    image(img,0,0,640,420);
+}
